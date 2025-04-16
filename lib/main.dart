261bba0bb46661f890ac382b808ef2a5dcc18c6c
LK-GONNA11 <seyzperly@gmail.com>
2025-04-16 18:09:30 +0200

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-
 import 'package:flutter/material.dart';
 import 'package:device_info_plus/device_info_plus.dart';
-import 'package:flutter/services.dart';
 
 void main() {
   runApp(const TurboNovaApp());
